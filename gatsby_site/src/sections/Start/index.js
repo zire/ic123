8d0525918c2d1cbe5c6e84ff12480861dce59636
Herbert <herbert.yang@dfinity.org>
2022-02-03 11:22:40 +0800

    Complete second section

--- gatsby_site/src/sections/Start/index.js
+++ gatsby_site/src/sections/Start/index.js
@@ -74,10 +74,43 @@ const ArticleStartPage = () => {
                         2. How To Do Research
                       </Text>
                       <Text className="entry-one">
-                        With all the above challenges. I have a few suggestions on how to do research, the right way in crypto. 
+                        With all the above challenges. I have a few suggestions on how to do research the right way when you want to learn about crypto.
+                      </Text>
+                      <Text className="entry-one">
+                        First, get on <a href="https://twitter.com/herbertyang">Twitter</a>. Everyone in crypto is on Twitter. After 20 years of social media, Twitter has now become the one and only place for intellectuals and KOLs in tech - it's certainly the largest. Instagram is for photo influencers. Reddit is an acquired taste with too many trolls. Facebook is for birthday reminders. Linkedin is a broken trainwreck where all the cool kids have already got off - it smells stale and looks utterly uninteresting. Reduce your time on local language site such as WeChat or Weibo to the minimum - they do have massive user base but generally have very, very low quality content that's not much better than opium. All the original content comes from Twitter. Try to read the source, not the second-hand Google-translated version. 
+                      </Text>
+                      <Text className="entry-one">
+                        Second, when you run into one blockchain project after another, always ask the question "which lockchain is this dApp built on?" Blockchain industry consists of several major tribes - or we call ecosystems. Every ecosystem is built around a Layer-0 or Layer-1 protocol. Each ecosystem has its own legions of crypto funds, developers, node providers, wallets, games, NFT collections, NFT marketplace and decentralized apps (dApps). 
+                      </Text>
+                      <Text className="entry-one">
+                        Third, do not make the naive assumption that Ethereum equals Blockchain. Ethereum is an influential protocol that has gathered huge amount of assets and almost all the crypto developers cut their teeth in the early days of Ethereum. However, there are many other blockchain protocols that are growing fast and may threaten the dominance of Ethereum. Bitcoin ecosystem is on its own - it can't do much other than being the store of value. Ethereum ecosystem is the dominance force where most of the NFT series are minted. Solana, Avalanche, NEAR, Polygon, Algorand, Binance Smart Contract (BSC) follow the footstep (and to varying degree, its code) of Ethereum and try to become a better version of Ethereum. Cardano is on their own planet. Polkadot and Cosmos are Layer-0 networks that try to connect multiple chains. The Internet Computer Protocol ("ICP") developed by DFINITY has expanded the boundary of blockchain industry significantly. Last but not the least, Arweave and Filecoin are two major IPFS networks that try to do distributed storage. If you come across an L0/1 blockchain that's not from this list, it's too small to matter.
+                      </Text>
+                      <Text className="entry-one">
+                        Forth, always apply first principle by asking these two questions: 1) "who are its customers?" and 2) "what value does it bring to its customers?". This is the only way to validate whether a blockchain protocol or the many dApps built on it, can bring value to the society. You will find that many blockchain protocols or dApps are created to support another protocol. Keep tracing up the chain until the end customer becomes the average Joe in the street. Try this exercise and see how many dApps/L1 blockchains can actually bring you to a real retail customer Joe or enterprise customer Smith.  
+                      </Text>
+                      <Text className="entry-one">
+                        Last, while it's natural to ask a few friends who seem to be closer to the crypto industry to get started, you should do your own research (DYOR). It's extremely difficult, if not impossible, to find a neutral voice in this industry. People are either too naive or too knee-deep in a camp already to tell you something objective - including myself. Read the white paper, conduct due dilligence on the team, observe how they conduct themselves on Twitter. Commit time to do this research and that's the only path to find truth or faith (always in relative terms) in crypto.
                       </Text>
                       <Text className="entry-three">
-                        3. What To Start
+                        3. Where To Start
+                      </Text>
+                      <Text className="entry-one">
+
+                      </Text>
+                      <Text className="entry-one">
+                        
+                      </Text>
+                      <Text className="entry-one">
+
+                      </Text>
+                      <Text className="entry-one">
+                        
+                      </Text>
+                      <Text className="entry-one">
+
+                      </Text>
+                      <Text className="entry-one">
+                        
                       </Text>
                       <Text className="entry-three">
                         4. How to Try Out ICP
